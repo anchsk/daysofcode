e5250e2834e0668e51e6551d2fc756f3956996ef
Anna <46578108+anchsk@users.noreply.github.com>
2021-01-08 14:25:00 +0100

    add loopstudio page

---
+++ components/Loopstudio/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export default function Loopstudio() {
+ return (
+  <Page>
+
+  </Page>
+ )
+}
+
+const Page = styled.div`
+width: 100%;
+height: 100%;`
---
+++ pages/loopstudio.js
@@ -0,0 +1,8 @@
+import Loopstudio from '../components/Loopstudio'
+
+export default function LoopstudioPage() {
+  return (
+      <Loopstudio />
+
+  )
+}
